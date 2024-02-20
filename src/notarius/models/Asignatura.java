638b7fa15c607ef0e883f51e228bd8c1748231ff
hanumonke <hanumansanchez@gmail.com>
2024-02-20 01:54:37 -0400

    arreglando el merge conflict con los controllers

--- src/notarius/models/Asignatura.java
+++ src/notarius/models/Asignatura.java
@@ -1,17 +1,9 @@
 
 package notarius.models;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import java.io.Serializable;
-=======
->>>>>>> a598430e2c927617a1dd89ce32bb4ce2af7cf94b
-=======
->>>>>>> a598430e2c927617a1dd89ce32bb4ce2af7cf94b
-=======
-import java.io.Serializable;
->>>>>>> hanu/JPAimpl
+
 import javax.persistence.Basic;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -19,19 +11,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 public class Asignatura implements Serializable {
-=======
-public class Asignatura {
->>>>>>> a598430e2c927617a1dd89ce32bb4ce2af7cf94b
-=======
-public class Asignatura {
->>>>>>> a598430e2c927617a1dd89ce32bb4ce2af7cf94b
-=======
-public class Asignatura implements Serializable {
->>>>>>> hanu/JPAimpl
+
      @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private String idAsignatura;
