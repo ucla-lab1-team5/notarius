3ec63d04cd325a8411b9e55aff6099548089e246
hanumonke <hanumansanchez@gmail.com>
2024-02-20 10:33:36 -0400

    DecanatoJpaController

--- src/notarius/controllers/DecanatoJpaController.java
+++ src/notarius/controllers/DecanatoJpaController.java
@@ -4,10 +4,28 @@
  */
 package notarius.controllers;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 /**
  *
  * @author hanumonke
  */
 public class DecanatoJpaController {
+
+    public void persist(Object object) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("notariusPU");
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        try {
+            em.persist(object);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+    }
     
 }
