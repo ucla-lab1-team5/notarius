940fc97c19ce2b6e436e8b8d17eddd9c944e2896
hanumonke <hanumansanchez@gmail.com>
2024-03-10 12:16:16 -0400

    corregidos otras urls a imagenes

--- src/main/java/ucla/lab/notarius/Notarius.java
+++ src/main/java/ucla/lab/notarius/Notarius.java
@@ -1,67 +1,22 @@
 /*
-<<<<<<< HEAD:src/notarius/Notarius.java
 Hanuman Sánchez CI: 28.316.086
 Anthony Moreno CI: 28.204.620
 Angel Goyo CI: 29.737.583
 Miller Arias CI: 29.561.941
 Luis Ochoa CI: 29.778.672
 */
-package notarius;
-=======
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package ucla.lab.notarius;
 
 import ucla.lab.notarius.views.Login;
->>>>>>> main:src/main/java/ucla/lab/notarius/Notarius.java
-
-import notarius.controllers.Controller;
-import notarius.views.Login;
-
-<<<<<<< HEAD:src/notarius/Notarius.java
-
 
 public class Notarius {
-    public static void main(String args[]) {
-        
-     //Setup de la app
-     //Agregar/Crear conexion
-     //Iniciar la db
-     //Controller control = new Controller();
-     
-    
-     // crear un admin user por defecto
-     
-     //creamos un decanato
-
-     
-     
-     //guardamos el decanato en la db
- 
-     
-     //creamos un segundo decanato
-
-    
-     
-     //guardamos el segundo decanato en la db
-     
-     
-     //control.editarDecanato(dah);
-     
-     //PROBANDO LAS VISTAS
-     Login loginScreen = new Login();
-     loginScreen.setVisible(true);
-     loginScreen.setLocationRelativeTo(null);
- 
-     
-=======
     public static void main(String[] args) {
 
         Login login = new Login();
         login.setVisible(true);
 
        
->>>>>>> main:src/main/java/ucla/lab/notarius/Notarius.java
+
     }
 }
